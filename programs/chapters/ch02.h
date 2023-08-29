618c71b0bb1aa2da329022f1751ffec288b3bd5d
Diogo Duarte <diogord@al.insper.edu.br>
2023-08-29 09:08:07 -0300

    Aula de Terça feita, adicionando constante kl

--- programs/chapters/ch02.h
+++ programs/chapters/ch02.h
@@ -3,10 +3,13 @@
 
 // Define o motor como saída PWM
 PwmOut motor1(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
 
 // Converte velocidade angular (rad/s) em sinal PWM (%)
 float control_motor(float omega){
-    float result = (1.4*pow(10, -7) * omega);
+    float result = a2*omega*omega;
     return result;
 }
 
@@ -14,14 +17,24 @@ float control_motor(float omega){
 int main(){
 
     // Frequência PWM para 500Hz
+    wait(2);
     motor1.period(1.0/500.0);
+    motor2.period(1.0/500.0);
+    motor3.period(1.0/500.0);
+    motor4.period(1.0/500.0);
 
-    // Liga o motor com velocidade angular de 1000 rad/s por 0.5 seg
-    motor1 = control_motor(1000.0);
-    wait(0.5);
+    // Liga o motor com velocidade angular de 1000 rad/s por 5 seg
+    motor1 = control_motor(2000.0);
+    motor2 = control_motor(2000.0);
+    motor3 = control_motor(2000.0);
+    motor4 = control_motor(2000.0);
+    wait(5);
 
     // Desliga o motor
     motor1 = 0.0;
+    motor2 = 0.0;
+    motor3 = 0.0;
+    motor4 = 0.0;
 
     //Fim do Programa
     while(true){
